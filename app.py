0cf89b3b85a3537ab041b0d999a1f4e2d69fe4d8
Adam Jandl <jandl.wisc@gmail.com>
2019-04-14 23:50:58 -0400

    Working flask app and template

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 from main import move_filtered_files
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return (
-        '<form action="http://localhost:5000/data"><input name="input1" type="text" /><input type="submit" /></form>'
-    )
+def home():
+    return render_template('index.html')
+
 
 @app.route('/data')
 def get_data():
-    data = request.args.get('input1')
-    return f'Got the data: {data}'
-
+    source_dir = request.args.get('input1')
+    target_dir = request.args.get('input2')
+    filter_str = request.args.get('input3')
+    try:
+        move_filtered_files(source_dir, target_dir, filter_str)
+    except:
+        return 'Something went wrong'
+    return 'Files moved successfully'
+    
 
 if __name__ == '__main__':
     app.run(debug=True)

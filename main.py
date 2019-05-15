344c7f823323ca9414e69cde2e80aff9ac9fb124
jandl.iqe <ajandl@iqep.com>
2019-05-15 17:42:47 -0400

    Setup get_filter_list and copy_filtered_files_multi.
    Tested get_filter_list

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from shutil import copyfile
 import argparse
 
 
+
 def find_files(source_dir):
     """
     function to traverse source dir for all files
@@ -52,25 +53,44 @@ def copy_files(found_files, targ_dir):
             print("Could not copy: ", f)
 
 
+def get_filter_list(filter_txt):
+    filter_list = []
+    with open(filter_txt) as f:
+        for l in f:
+            l_parsed = l.rstrip().replace('-', '')
+            filter_list.append(l_parsed)
+    
+    return filter_list
+
 def move_filtered_files(source_dir, target_dir, filter_str):
     all_files = find_files(source_dir)
     found_files = filter_files(filter_str, all_files)
     copy_files(found_files, target_dir)
 
 
-def main(source_dir, target_dir, filter_str):
-    move_filtered_files(source_dir, target_dir, filter_str)
-    # move_filtered_files(
-    #     r"C:\Users\jandl\OneDrive\Documents\Python Projects\File searcher",
-    #     r'C:\Users\jandl\OneDrive\Documents\Python Projects\File searcher\feed_me',
-    #     "apple"
-    # )
+def copy_filtered_files_multi(source_dir, target_dir, filter_list):
+    for filter_str in filter_list:
+        move_filtered_files(source_dir, target_dir, filter_str)
+
+
+# def main(source_dir, target_dir, filter_str):
+#     # move_filtered_files(source_dir, target_dir, filter_str)
+#     # move_filtered_files(
+#     #     r"C:\Users\jandl\OneDrive\Documents\Python Projects\File searcher",
+#     #     r'C:\Users\jandl\OneDrive\Documents\Python Projects\File searcher\feed_me',
+#     #     "apple"
+#     # )
+
+def main():
+    filter_list = get_filter_list(r"\\group.iqep.com\ma\user\ajandl\My Documents\Data\For customer\Lasertel\19-05-15 More Surfscan\filter_list.txt")
+    print(filter_list)
 
     
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('source_dir', help="Define the directory with files to work on")
-    parser.add_argument('target_dir', help="Define the target directory")
-    parser.add_argument('filter_str')
-    args = parser.parse_args()
-    main(args.source_dir, args.target_dir, args.filter_str)
+    main()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('source_dir', help="Define the directory with files to work on")
+    # parser.add_argument('target_dir', help="Define the target directory")
+    # parser.add_argument('filter_str')
+    # args = parser.parse_args()
+    # main(args.source_dir, args.target_dir, args.filter_str)

9e96099a150c648cc79e0e715329c24afcf866a0
Adam Jandl <jandl.wisc@gmail.com>
2019-05-14 23:43:15 -0400

    Changed to using POST requests

--- app.py
+++ app.py
@@ -10,16 +10,21 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/data')
+@app.route('/data', methods=['POST'])
 def get_data():
-    source_dir = request.args.get('input1')
-    target_dir = request.args.get('input2')
-    filter_str = request.args.get('input3')
+    """Args is a dict, we are using the get method instead of the [] 
+       so that it will return 'none' if key is missing/undefined."""
+    data = request.form
+    source_dir = data.get('input1')
+    target_dir = data.get('input2')
+    filter_str = data.get('input3')
     try:
-        move_filtered_files(source_dir, target_dir, filter_str)
+        # move_filtered_files(source_dir, target_dir, filter_str)
+        return '{source}, {target}, {filter}' \
+            .format(source=source_dir, target=target_dir, filter=filter_str)
     except:
         return 'Something went wrong'
-    return 'Files moved successfully'
+    # return 'Files moved successfully'
     
 
 if __name__ == '__main__':
